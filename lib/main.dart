4a44e187a10851abc3a0e90b8572144e019247f0
Salvo <salvatoresalanitro94@gmail.com>
2025-06-06 11:35:38 +0200

    create draft layout to expenses class

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:expense_tracker_app/expenses.dart';
 
 void main(){
   runApp(
     MaterialApp(
-      home: Text('Welcome'),
+      home: Expenses(),
     ),
   );
 }
