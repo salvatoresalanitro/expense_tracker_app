cafff01e3305b909ea571b71a745b6be55a6ef3d
Salvo <salvatoresalanitro94@gmail.com>
2025-06-06 17:56:56 +0200

    add category icons new class constants to map categories

---
+++ lib/models/category_icons.dart
@@ -0,0 +1,9 @@
+import 'package:expense_tracker_app/models/category.dart';
+import 'package:flutter/material.dart';
+
+const categoryIcons = {
+  Category.food: Icons.lunch_dining,
+  Category.travel: Icons.flight_takeoff,
+  Category.leisure: Icons.movie,
+  Category.work: Icons.work,
+};
