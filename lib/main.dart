5ee5d9f8d0bee85bb22732a5940afec8c1b10592
Salvo <salvatoresalanitro94@gmail.com>
2025-06-06 18:16:59 +0200

    add intl and flutter_localization to localize other countries

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:expense_tracker_app/widgets/expenses.dart';
+import 'package:intl/date_symbol_data_local.dart';
 
 void main(){
+  initializeDateFormatting();
+
   runApp(
     MaterialApp(
       home: Expenses(),
