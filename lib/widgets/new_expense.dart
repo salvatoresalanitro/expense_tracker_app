af40c9678d34c6a23e55dd23bfab160b8cc5a1d3
Salvo <salvatoresalanitro94@gmail.com>
2025-06-08 11:12:36 +0200

    set new_expense.dart widget with 2 controllers and layout

--- lib/widgets/new_expense.dart
+++ lib/widgets/new_expense.dart
@@ -10,18 +10,53 @@ class NewExpense extends StatefulWidget {
 }
 
 class _NewExpenseState extends State<NewExpense> {
+  final _titleController = TextEditingController();
+  final _amountController = TextEditingController();
+
+  @override
+  void dispose() {
+    _titleController.dispose();
+    _amountController.dispose();
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Padding(
       padding: const EdgeInsets.all(16),
       child: Column(
-        children: const [
+        children: [
           TextField(
+            controller: _titleController,
             maxLength: 50,
             decoration: InputDecoration(
               label: Text('Title'),
             ),
-          )
+          ),
+          SizedBox(height: 8),
+          TextField(
+            controller: _amountController,
+            keyboardType: TextInputType.number,
+            decoration: InputDecoration(
+              prefixText: '€',
+              label: Text('Amount'),
+            ),
+          ),
+          Row(
+            children: [
+              TextButton(
+                onPressed: () {},
+                child: Text('Cancel'),
+              ),
+              ElevatedButton(
+                onPressed: () {
+                  print(_titleController.text);
+                  print(_amountController.text);
+                },
+                child: Text('Save expense'),
+              ),
+            ],
+          ),
         ],
       ),
     );
