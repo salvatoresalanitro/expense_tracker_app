59d28bd7f8477c63f30598dd6d6d71322f65337f
Salvo <salvatoresalanitro94@gmail.com>
2025-06-08 17:32:57 +0200

    add a way to undo wrong deleted expenses

--- lib/widgets/expenses.dart
+++ lib/widgets/expenses.dart
@@ -31,7 +31,8 @@ class _ExpensesState extends State<Expenses> {
     showModalBottomSheet(
       isScrollControlled: true,
       context: context,
-      builder: (ctx) => NewExpense(onAddExpense: _addExpense,),
+      builder:
+          (ctx) => NewExpense(onAddExpense: _addExpense),
     );
   }
 
@@ -42,9 +43,25 @@ class _ExpensesState extends State<Expenses> {
   }
 
   void _removeExpense(Expense expense) {
+    final expenseIndex = _registeredExpenses.indexOf(expense);
     setState(() {
       _registeredExpenses.remove(expense);
     });
+    ScaffoldMessenger.of(context).clearSnackBars();
+    ScaffoldMessenger.of(context).showSnackBar(
+      SnackBar(
+        duration: const Duration(seconds: 3),
+        content: const Text('Expense deleted'),
+        action: SnackBarAction(
+          label: 'Undo',
+          onPressed: () {
+            setState(() {
+              _registeredExpenses.insert(expenseIndex, expense);
+            });
+          },
+        ),
+      ),
+    );
   }
 
   @override
@@ -53,7 +70,7 @@ class _ExpensesState extends State<Expenses> {
       child: Text('No expense found. Start adding some!'),
     );
 
-    if(_registeredExpenses.isNotEmpty) {
+    if (_registeredExpenses.isNotEmpty) {
       mainContent = ExpensesList(
         expenses: _registeredExpenses,
         onRemoveExpense: _removeExpense,
@@ -73,9 +90,7 @@ class _ExpensesState extends State<Expenses> {
       body: Column(
         children: [
           const Text('The chart'),
-          Expanded(
-            child: mainContent,
-          ),
+          Expanded(child: mainContent),
         ],
       ),
     );
