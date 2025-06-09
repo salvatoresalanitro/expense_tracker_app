6a88d3482423f436c37a5d61f4e3c5c3b881cc6b
Salvo <salvatoresalanitro94@gmail.com>
2025-06-09 10:01:55 +0200

    rename file and add constructor to filter specific category

--- lib/models/expense_bucket.dart
+++ lib/models/expense_bucket.dart
@@ -1,19 +1,27 @@
 import 'package:expense_tracker_app/models/category.dart';
 import 'package:expense_tracker_app/models/expense.dart';
 
-class Expensebucket {
-  const Expensebucket({required this.category, required this.expenses});
+class ExpenseBucket {
+  const ExpenseBucket({
+    required this.category,
+    required this.expenses,
+  });
 
- final Category category;
- final List<Expense> expenses;
+  ExpenseBucket.forCategory(List<Expense> allExpenses,this.category,)
+    : expenses = allExpenses
+      .where((expense) => expense.category == category)
+      .toList();
 
- double get totalExpense {
-  double sum = 0;
+  final Category category;
+  final List<Expense> expenses;
 
-  for(final expense in expenses) {
-    sum += expense.amount;
+  double get totalExpense {
+    double sum = 0;
+
+    for (final expense in expenses) {
+      sum += expense.amount;
+    }
+
+    return sum;
   }
-
-  return sum;
- }
 }
