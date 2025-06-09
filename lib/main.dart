defa53c5173e76047f20d4d84f6b8a6fb88a7796
Salvo <salvatoresalanitro94@gmail.com>
2025-06-09 08:58:52 +0200

    add main theme to application layout

--- lib/main.dart
+++ lib/main.dart
@@ -3,15 +3,45 @@ import 'package:expense_tracker_app/widgets/expenses.dart';
 import 'package:intl/date_symbol_data_local.dart';
 import 'package:flutter_localizations/flutter_localizations.dart';
 
-void main(){
+var kColorScheme = ColorScheme.fromSeed(
+  seedColor: const Color.fromARGB(255, 180, 242, 110),
+);
+
+void main() {
   initializeDateFormatting();
 
   runApp(
     MaterialApp(
+      theme: ThemeData().copyWith(
+        colorScheme: kColorScheme,
+        appBarTheme: const AppBarTheme().copyWith(
+          backgroundColor: kColorScheme.onPrimaryContainer,
+          foregroundColor: kColorScheme.primaryContainer,
+        ),
+        cardTheme: CardTheme().copyWith(
+          color: kColorScheme.secondaryContainer,
+          margin: EdgeInsets.symmetric(
+            horizontal: 16,
+            vertical: 8,
+          ),
+        ),
+        elevatedButtonTheme: ElevatedButtonThemeData(
+          style: ElevatedButton.styleFrom(
+            backgroundColor: kColorScheme.primaryContainer,
+          ),
+        ),
+        textTheme: ThemeData().textTheme.copyWith(
+          titleLarge: TextStyle(
+            fontWeight: FontWeight.normal,
+            color: kColorScheme.onSecondaryContainer,
+            fontSize: 14,
+          ),
+        ),
+      ),
       home: Expenses(),
       localizationsDelegates: [
         GlobalMaterialLocalizations.delegate,
-        GlobalWidgetsLocalizations.delegate
+        GlobalWidgetsLocalizations.delegate,
       ],
     ),
   );
